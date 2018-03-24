e31eee1871d1db19b5acd1bb1c3b615bd595c89c
NicklasFrederiksen <nfrederiksen@hotmail.com>
2018-03-25 00:35:22 +0100

    Init

---
+++ PartyGameVR/Assets/PassTheBomb.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassTheBomb : MonoBehaviour {
+
+    public bool gameStarted = false;
+    bool isBombInPlay = false;
+
+    private void Update() {
+        if (!gameStarted) return;
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+
+        }
+    }
+
+    public void StartGame() {
+        gameStarted = true;
+        AddBomb();
+    }
+
+    void AddBomb() {
+        isBombInPlay = true;
+        print("Placér bombe");
+    }
+
+
+}
---
+++ PartyGameVR/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+public class PlayerController : MonoBehaviour {
+
+    public InputDevice Device { get; set; }
+
+    bool hasTheBomb = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
